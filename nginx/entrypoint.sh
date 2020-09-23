b40e631e2885bfd0bab46de711b46a131ab25bc1
Simon Paulger <simon.paulger@deskpro.com>
2020-09-23 13:16:45 +0100

    Support templating of environment variables starting DESKPRO_
    Support the use of templating using envsubst with all environment
    variables that start with DESKPRO_. This is a compromise of allowing
    easily passing environment variables and not having to escape every use
    of dollar symbol in nginx site vhost files.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,6 +6,15 @@ export WORKER_PROCESSES=${WORKER_PROCESSES:-"auto"}
 
 rm -f "/etc/nginx/sites-enabled/default"
 
+# Produce a string list of each exported environment
+# variable that has a name starting with DESKPRO_
+exported_vars=$(env | sed -n 's/^\(DESKPRO_.*\)=.*/\1/p')
+deskpro_var_list=""
+
+for var in $exported_vars; do
+  deskpro_var_list="$deskpro_var_list \$${var} "
+done
+
 # Generate the nginx master configuration
 # file from the template file
 envsubst '$WORKER_PROCESSES' < "/etc/nginx/nginx.conf.template" > "/etc/nginx/nginx.conf"
@@ -16,7 +25,7 @@ for template_file in /etc/nginx/sites-available/*.template; do
   [ -f "$template_file" ] || continue
 
   config_file=$(echo "$template_file" | sed -e 's/\.template$//')
-  envsubst '$VIRTUAL_HOST $FASTCGI_HOST' < "$template_file" > "$config_file"
+  envsubst "\$VIRTUAL_HOST \$FASTCGI_HOST $deskpro_var_list" < "$template_file" > "$config_file"
 
   config_filename=$(basename "$config_file")
   rm -f "/etc/nginx/sites-enabled/$config_filename"
