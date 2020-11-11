49115a33788604ad98746aac1e2d40043fd2fb5f
Ashley Dawson <ashley.dawson@deskpro.com>
2020-11-11 16:55:20 +0000

    Added dual purpose backup restoration container

---
+++ backup-restore/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Configure mysql client
+cat > ~/.my.cnf << EOF
+[client]
+host=$MYSQL_HOST
+user=$MYSQL_USER
+password=$MYSQL_PASS
+EOF
+
+chmod 600 ~/.my.cnf
+
+# Unzip the DB dump and pipe into MySQL
+unzip -p "${BACKUP_ARTIFACT_PATH}" "${BACKUP_DB_DUMP_NAME}" | mysql "${MYSQL_DBNAME}"
+
+# If archive contains attachments, install them
+unzip -l "${BACKUP_ARTIFACT_PATH}" | grep -q "$(basename "${ATTACHMENTS_PATH}")"
+if [ "$?" = "0" ] ; then
+    unzip "${BACKUP_ARTIFACT_PATH}" "$(basename "${ATTACHMENTS_PATH}")/*" -d "${ATTACHMENTS_PATH}/../"
+fi
